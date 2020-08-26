65ff0016b885f9d59f9a8aef4ea235edd8d27ad0
robert-cummins <robert.k.cummins@gmail.com>
2020-08-27 11:58:52 +1200

    make update to article table with new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_233852) do
+ActiveRecord::Schema.define(version: 2020_08_26_235223) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
